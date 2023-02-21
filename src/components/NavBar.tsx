f23db49d6a53d6411d19c7cf4478ed767e365715
ackerman <julio.sergio2709@gmail.com>
2023-02-21 01:02:09 -0500

    Update: NavBar margins and paddings

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,10 +3,11 @@ import FindProducts from "./FindProducts";
 
 const NavBar: React.FC = () => {
   return (
-    <nav className="my-5 mx-auto flex h-auto w-11/12 items-center justify-between rounded-2xl px-4 py-3">
-      <div></div>
-      <FindProducts />
-      <div className="flex justify-between">
+    <nav className="my-5 mx-auto flex h-auto w-11/12 items-center justify-around rounded-2xl">
+      <div className="mx-3 ml-5">
+        <FindProducts />
+      </div>
+      <div className="mx-2 flex min-w-[80px] justify-between">
         <div className="mr-3">
           <InstagramIcon stroke="#555555" height={18} />
         </div>
