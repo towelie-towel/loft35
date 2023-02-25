9acae9ad160fbe4472536f2df04821e047cd3ad0
ackerman <julio.sergio2709@gmail.com>
2023-02-24 21:03:04 -0500

    changing font colors again

--- src/components/Product_Categories.tsx
+++ src/components/Product_Categories.tsx
@@ -5,14 +5,14 @@ interface IProps {
 const ProductCategories: React.FC<IProps> = ({ categories }) => {
   return (
     <div className="flex w-full flex-col">
-      <div className="ml-10 pb-4 text-[1rem] font-semibold text-[var(--font-secondary-color)]">
+      <div className="ml-10 pb-4 text-[1rem] font-semibold">
         <h2>Productos disponibles</h2>
       </div>
-      <div className="items-center overflow-x-scroll whitespace-nowrap px-0 pb-3 text-[var(--font-secondary-color)]">
+      <div className="items-center overflow-x-scroll whitespace-nowrap px-0 pb-3">
         {categories.map((category) => {
           return (
             <h2
-              className="mx-3 inline-block overflow-hidden rounded-md bg-[var(--primary-bg-color)] px-4 font-semibold shadow-2xl"
+              className="mx-2 inline-block overflow-hidden rounded-md bg-[var(--primary-bg-color)] py-1 px-4 font-semibold shadow-2xl"
               key={category.join()}
             >
               {category.join(" & ")}
