bf1251383342dd6eaa6e0ed083720c0e875b33fc
ackerman <julio.sergio2709@gmail.com>
2023-03-16 16:53:54 -0400

    Add: sizes propertie to carSlider images

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -21,7 +21,15 @@ export const Card: React.FC<CardProps & Record<string, unknown>> = ({
       className="flex w-[var(--slider-width)] flex-col items-center justify-center"
     >
       <div className="relative w-[90%] rounded-md pb-[25%] shadow-lg">
-        <Image priority src={src} alt={name} fill />
+        <Image
+          sizes="(max-width: 425px) 50vw,
+              (max-width: 768px) 75vw,
+              (max-width: 1024px) 100vw"
+          priority
+          src={src}
+          alt={name}
+          fill
+        />
       </div>
     </div>
   );
