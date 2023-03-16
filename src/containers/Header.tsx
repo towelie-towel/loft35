dca4d58b394ad7eb971ba7d579438878fea42160
ackerman <julio.sergio2709@gmail.com>
2023-03-16 14:53:27 -0400

    Add: ImageUploaderForm to Header

--- src/containers/Header.tsx
+++ src/containers/Header.tsx
@@ -1,11 +1,13 @@
 import HeaderView from "../components/Header_View";
 import NavBar from "../components/NavBar";
+import ImageUploaderForm from "../components/UploadProduct";
 
 const Header = () => {
   return (
     <header className="container mx-auto">
       <NavBar />
       <HeaderView />
+      <ImageUploaderForm />
     </header>
   );
 };
